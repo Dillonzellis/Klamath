2e69a91a656d51ef5ec3c072be9a164a7dd6eb4b
Dillon Ellis <dellis@fcul.com>
2022-05-16 14:27:44 -0400

    remove road trip banner

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Klamath Public Employees FCU - Home</title>
+    <meta name="description" content="Klamath Public Employees Federal Credit Union was originally known as Klamath Falls United States Employees and served only employees of the Federal Government.">
+    <meta name="keywords" content="Klamath Public Employees Federal Credit Union, KPEFCU, Klamath Falls, credit union, federal credit union, Klamath Falls Credit Union">
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Font Code -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,700i,800%7COverlock:400,400i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,vietnamese" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link href="css/combine-test.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styleTest.css" />
+    <!-- Remodal styles -->
+    <link rel="stylesheet" href="remodal/dist/remodal.css">
+    <link rel="stylesheet" href="remodal/dist/remodal-default-theme.css">
+
+    <script src="js/modernizr.custom.js"></script>
+    <!--[if lt IE 9]><script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!-- Favicon and Apple Icons -->
+    <link rel="shortcut icon" href="images/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="images/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-icon-114x114.png">
+</head>
+
+<body id="top">
+
+    <!-- ::: START HEADER ::: data-spy="affix" -->
+    <header id="home" class="header2" data-offset-top="175">
+        <div class="container">
+            <div class="row">
+                <?php include 'inc/header.php'; ?>
+            </div><!-- end row -->
+        </div><!-- end container -->
+        <p style="padding:15px; background:yellow; font-size: 18px;" align="center"><strong>Virtual Branch, Mobile App and Audio Response will be down for system maintenance starting at 5:00 PM, Monday May 16th. We hope to have these services available within a couple of hours. We apologize for any inconvenience this may cause.</strong></p>
+    </header>
+    <!-- ::: END ::: -->
+
+    <!--  :::  HOME SLIDER ::: -->
+    <div id="image-slider" class="carousel slide carousel-fade" data-ride="carousel">
+
+        <!-- Indicators -->
+        <!--<ol class="carousel-indicators">
+    	<li data-target="#image-slider" data-slide-to="0" class="active"></li>
+    	<li data-target="#image-slider" data-slide-to="1"></li>
+    	<li data-target="#image-slider" data-slide-to="2"></li>
+  	</ol>-->
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+
+            <!-- start carousel item -->
+            <!--<div class="item rightG active">
+    		<div class="grad"></div>
+    		<img class="img-responsive" src="images/slider/rvfun.jpg" alt="rv fun banner" />
+      		<div class="carousel-caption">
+           		<h1 data-animation="animated fadeIn">April RV Loan Special</h1>
+           		<p data-animation="animated fadeIn" style="margin-bottom: 15px; ">New, Used and Re-Financed RV and Travel Trailer Rates as low as <br>4.5% O.A.C. and Boat rates as low as 3.5% O.A.C. <br>
+					<span style="font-size: 24px; color: white;"><strong>Call or come by today!</span></strong></p>
+				<p data-animation="animated fadeIn"><a class="btn btn-prime" href="https://www.kpefcu.com/pages/auto-loan.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+      		</div>
+    	</div>-->
+            <!-- end carousel item -->
+
+            <!-- start carousel item -->
+            <!--<div class="item rightG active">
+    		<div class="grad"></div>
+    		<img class="img-responsive" src="images/slider/rvfun.jpg" alt="July rv special loan Available" />
+      		<div class="carousel-caption">
+           		<h1 data-animation="animated fadeIn">July loan special.</h1>
+           		<p data-animation="animated fadeIn">Auto rates as low as 1% O.A.C., RV Rates as low as 4% O.A.C. <br>and Boats as low as 3% O.A.C.</p>
+				<!--<p data-animation="animated fadeIn"><a class="btn btn-prime" href="https://www.kpefcu.com/pages/auto-loan.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+      		</div>
+    	</div>-->
+
+            <!-- start carousel item -->
+            <!-- <div class="item leftG active">
+                <div class="grad"></div>
+                <img class="img-responsive" src="images/slider/summer-rv.jpg" alt="Recreational Vehicle Loan" />
+                <div class="carousel-caption">
+                    <h1 data-animation="animated fadeIn">Take your road<br>trip up a notch!</h1>
+                    <p data-animation="animated fadeIn">RV rates including all Travel Trailer, Toy Hauler, Camper and<br>Motorhome’s as low as 3.5% o.a.c. and Boat rates as low as 2.5% o.a.c.</p>
+                    <p data-animation="animated fadeIn"><a class="btn btn-prime" href="https://www.kpefcu.com/pages/rec-vehicle.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+                </div> -->
+                <!-- carousel caption -->
+            <!-- </div> -->
+            <!-- end carousel item -->
+
+            <!-- start carousel item -->
+            <!-- <div class="item rightG">
+                <div class="grad"></div>
+                <img class="img-responsive" src="images/slider/heloc.jpg" alt="Home Equity Line of Credit" />
+                <div class="carousel-caption">
+                    <h1 data-animation="animated fadeIn">Did you know we offer Home Equity Line of Credit loans?</h1>
+                    <p data-animation="animated fadeIn">HELOC's are a great way to use funds from the equity in your home for various different updates or purchases. Come and see us today for all your home improvement needs.</p>
+                    <p data-animation="animated fadeIn"><a class="btn btn-prime" href="pages/home-loan.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+                </div> -->
+                <!-- carousel caption -->
+            <!-- </div> -->
+            <!-- end carousel item -->
+
+            <!-- start carousel item -->
+            <!--<div class="item leftG">
+    		<div class="grad"></div>
+    		<img class="img-responsive" src="images/slider/rv-loan.jpg" alt="Recreational Vehicle Loan" />
+      		<div class="carousel-caption">
+           		<h1 data-animation="animated fadeIn">Start your summer out right with a Recreational Vehicle loan!</h1>
+           		<p data-animation="animated fadeIn">We offer low rates and extended terms out to 180 months.<br>Apply online or swing by in person.</p>
+				<p data-animation="animated fadeIn"><a class="btn btn-prime" href="https://www.kpefcu.com/pages/rec-vehicle.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+      		</div>
+    	</div>-->
+            <!-- end carousel item -->
+
+            <!-- start carousel item -->
+            <div class="item centerG">
+                <div class="grad2"></div>
+                <img class="img-responsive" src="images/slider/notice.jpg" alt="Fraud Notice" />
+                <div class="carousel-caption">
+                    <h1 data-animation="animated fadeIn">IT'S A SCAM!</h1>
+                    <p data-animation="animated fadeIn">Each year, scam artists and identity thieves steal billions of dollars from unsuspecting consumers. They use the telephone, email, text messaging, postal mail and the internet to steal information or trick consumers into handing over money. As we are seeing an influx of fraudulent transactions to our members, please be cautious and don’t give out any personal or banking information. If something seems fishy, hang up and call the Credit Union directly at 541-882-5525.</p>
+                </div><!-- carousel caption -->
+            </div>
+            <!-- end carousel item -->
+
+
+            <!-- start carousel item -->
+            <div class="item rightG">
+                <div class="grad"></div>
+                <img class="img-responsive" src="images/slider/auto-loan.jpg" alt="Auto Loans Available" />
+                <div class="carousel-caption">
+                    <h1 data-animation="animated fadeIn">Get your dream car today!</h1>
+                    <p data-animation="animated fadeIn">For more information on rates or to apply click below.</p>
+                    <p data-animation="animated fadeIn"><a class="btn btn-prime" href="https://www.kpefcu.com/pages/auto-loan.php">Learn More Now <i class="fa fa-angle-double-right"></i></a></p>
+                </div><!-- carousel caption -->
+            </div>
+            <!-- end carousel item -->
+
+
+        </div><!-- start carousel inner -->
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#image-slider" data-slide="prev"><span style="display:none;">Previous</span>
+            <img class="svg" src="images/svg/arrow-left-w.svg" onerror="this.src='images/svg/arrow-left-w.png'" alt="Prev" />
+        </a>
+        <a class="right carousel-control" href="#image-slider" data-slide="next"><span style="display:none;">Next</span>
+            <img class="svg" src="images/svg/arrow-right-w.svg" onerror="this.src='images/svg/arrow-right-w.png'" alt="Next" />
+        </a>
+    </div>
+    <!-- ::: END HOME SLIDER  ::: -->
+
+    <!--  :::  START FEATURED SERVICES ::: -->
+    <section class="page-block-small featuredServices">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="box box1">
+                        <h3>CALCULATORS</h3>
+                        <p>Need help with some financial calculations? We may have a calculator that can help.</p>
+                        <a class="abs btn btn-service1" href="pages/calculators.php">Learn More <i class="fa fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="box box2">
+                        <h3>NEWS</h3>
+                        <p>Check out what is happening at the Credit Union!<br><br></p>
+                        <a class="abs btn btn-service2" href="pages/news.php">Read More <i class="fa  fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="box box3">
+                        <h3>MEMBERSHIP</h3>
+                        <p>Interested in becoming a member? See how you could qualify.</p>
+                        <a class="abs btn btn-service3" href="https://www.kpefcu.com/pages/who-can-join.php">Join Today <i class="fa  fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </section>
+    <!-- ::: END FEATURED SERVICES ::: -->
+
+
+    <!--  :::  START COMBO ::: -->
+    <section class="combo">
+
+        <div class="fancyBorder top one"></div>
+
+
+        <div class="opaqueB">
+            <div class="container">
+                <div class="row">
+
+                    <div class="heading">
+                        <h1 class="center" style="color: #fff;">Our <strong style="color: #fff;">Loan Types</strong><br><small>wide variety of loans</small></h1>
+                    </div>
+
+                    <!--  ::: start services ::: -->
+                    <section class="servicesList page-block oflowVisible">
+                        <div class="container">
+
+
+                            <!-- start service -->
+                            <div class="listBox">
+                                <div class="image">
+                                    <a href="pages/auto-loan.php"><img class="img-responsive" src="images/services/small/auto-loan.jpg" width="150" height="150" alt="Auto Loans" /></a>
+                                </div><!-- end Image -->
+                                <div class="content">
+                                    <h4><a href="pages/auto-loan.php">Auto Loan</a></h4>
+                                    <p>In the market for a new or used car or need to refinance? We have great rate and terms available.</p>
+                                </div><!-- end Content -->
+                                <div class="clear"></div>
+                            </div>
+                            <!-- end service -->
+
+                            <!-- start service -->
+                            <div class="listBox">
+                                <div class="image">
+                                    <a href="pages/home-loan.php"><img class="img-responsive" src="images/services/small/home-loan.jpg" width="150" height="150" alt="Auto Loans" /></a>
+                                </div><!-- end Image -->
+                                <div class="content">
+                                    <h4><a href="pages/home-loan.php">Home Loans</a></h4>
+                                    <p>Looking to purchase a new home or remodel your existing home, we have a loan for that!</p>
+
+                                </div><!-- end Content -->
+                                <div class="clear"></div>
+                            </div>
+                            <!-- end service -->
+
+                            <!-- start service -->
+                            <div class="listBox">
+                                <div class="image">
+                                    <a href="pages/student-loans.php"><img class="img-responsive" src="images/services/small/student-loan.jpg" width="150" height="150" alt="Student Loans Image" /></a>
+                                </div><!-- end Image -->
+                                <div class="content">
+                                    <h4><a href="pages/student-loans.php">Student Loans</a></h4>
+                                    <p>Let us help you with those higher education costs.<br><br></p>
+
+                                </div><!-- end Content -->
+                                <div class="clear"></div>
+                            </div>
+                            <!-- end service -->
+
+                            <!-- start service -->
+                            <div class="listBox">
+                                <div class="image">
+                                    <a href="pages/credit-cards.php"><img class="img-responsive" src="images/services/small/credit-cards.jpg" width="150" height="150" alt="Auto Loans" /></a>
+                                </div><!-- end Image -->
+                                <div class="content">
+                                    <h4><a href="pages/credit-cards.php">Credit Cards</a></h4>
+                                    <p>Interested in a low rate credit card? We have two great options.</p>
+
+                                </div><!-- end Content -->
+                                <div class="clear"></div>
+                            </div>
+                            <!-- end service -->
+
+                        </div><!-- end container -->
+                    </section><!-- ::: end services ::: -->
+
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </div><!-- end opaqueB -->
+    </section>
+    <!-- ::: END COMBO ::: -->
+
+
+    <!--  :::  FOOTER ::: -->
+    <footer id="footer1" class="BGdark page-block-small">
+        <?php include 'inc/footer.php'; ?>
+    </footer>
+    <!-- ::: END FOOTER ::: -->
+
+    <a href="#top" class="top BGprime"><i class="fa fa-angle-up fa-lg"><span style="display: none;">Back to Top Button</span></i></a>
+    <?php include 'inc/modals.php'; ?>
+    <!--START SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/scripts.min.js"></script>
+    <script src="js/master.js"></script>
+    <script src="remodal/dist/remodal.js"></script>
+</body>
+
+</html>
